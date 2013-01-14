09435e6409773fa2acf2b51d2ca7f1ed458edb7b
chrishensel <futurefwarrior@googlemail.com>
2013-01-14 18:03:02 +0100

    Added log

---
+++ Tools/AlarmWorkflow.AutoUpdater/Views/LogControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace AlarmWorkflow.Tools.AutoUpdater.Views
+{
+    /// <summary>
+    /// Interaction logic for LogControl.xaml
+    /// </summary>
+    public partial class LogControl : UserControl
+    {
+        #region Constructors
+
+        public LogControl()
+        {
+            InitializeComponent();
+
+            foreach (string text in Log.Entries)
+            {
+                AddToLog(text);
+            }
+            Log.PostText += Log_PostText;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void AddToLog(string text)
+        {
+            txtLog.AppendText(text + Environment.NewLine);
+        }
+
+        void Log_PostText(string text)
+        {
+            txtLog.Dispatcher.BeginInvoke((Action)(() => AddToLog(text)));
+        }
+
+        #endregion
+    }
+}
