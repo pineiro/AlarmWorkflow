96aa2ad05b877a4e92e10007f01950a2f67982b0
chrishensel <futurefwarrior@googlemail.com>
2013-01-22 14:48:54 +0100

    Added basic Installer for dev-dummy-pkg

---
+++ Tools/AlarmWorkflow.Tools.UpdaterDummyPackage/Installer.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * See "(Root)/Resources/Versioning/InstallerClassTemplate/InstallerTemplate.cs" for documentation.
+ */
+
+using System.Collections.Generic;
+class Installer
+{
+    string[] OnInstall(
+        string installDirectory,
+        System.Version oldVersion,
+        System.Version newVersion)
+    {
+        List<string> log = new List<string>();
+        log.Add("iSuccess!");
+
+        return log.ToArray();
+    }
+}
